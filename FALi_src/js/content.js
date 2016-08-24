0f8df898b742e4489566d1b64c768d24edba62b1
qwwdezone <qww.zone@aliyun.com>
2016-08-24 15:14:13 +0800

    remove browser.min.js

--- FALi_src/js/content.js
+++ FALi_src/js/content.js
@@ -1,5 +1,29 @@
 $(function(){
-addDivElement();
+  //在 对应页面 类选择器.tb-meta 插入DIV块。 三目运算符
+  -1!=window.location.href.indexOf("detail.tmall.com")||
+  -1!=window.location.href.indexOf("detail.yao.95095.com")||
+  -1!=window.location.href.indexOf("world.tmall.com")?
+  ($(".tb-meta").prepend('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("item.taobao.com")?
+  ($(".tb-meta").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("world.taobao.com")?
+  ($("#J_TbcPresale").before('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("detail.ju.taobao.com")?
+  ($(".description").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("detail.tmall.hk")?
+  ($(".tm-fcs-panel").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("items.alitrip.com")?
+  ($(".detail-ind").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("hotel.alitrip.com")?
+  ($(".hotel-meta").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("item.alitrip.com")?
+  ($(".tb-meta").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("detail.alitrip.hk")?
+  ($(".tm-fcs-panel").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  -1!=window.location.href.indexOf("tuan.alitrip.com")?
+  ($(".tm-ind-panel").after('<div id="fali" style="margin-top:10px;margin-bottom:5px"></div>')):
+  1;
+  addDivElement();
 });
 
 //获取各个数据，渲染网页
@@ -19,6 +43,6 @@ var RenderElements = React.createClass({
 //根据淘宝，天猫，阿里旅行等网址不同，查找网页对应位置添加 DIV 区块。
 function addDivElement(){
   ReactDOM.render(React.createElement(RenderElements, { name: "John" }),
-  document.getElementById('J_SiteNav')
+  document.getElementById('fali')
   );
 }
