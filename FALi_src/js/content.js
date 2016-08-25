fb7b5629de822ddc9abd8d85eaf285390c8a45cc
qwwdezone <qww.zone@aliyun.com>
2016-08-25 16:57:55 +0800

    modify content.js style.css 2016-08-25:16:57

--- FALi_src/js/content.js
+++ FALi_src/js/content.js
@@ -58,13 +58,53 @@ chrome.runtime.sendMessage(
         var date_prices = date_prices[0]; //获取到第一个匹配到的串 包含了日期  和 价格。
         date_prices=date_prices.replace("points : [[","");
         date_prices=date_prices.replace("]]};","");
-        var day_price_array=date_prices.split("],[");
+        var day_price_array=date_prices.split("],["); //格式：  '2016-08-24',58 的数组
+
+        var day_item_array =  new Array();  //2016-08-24  日期数组
+        var price_item_array = new Array(); // 单日价格数组
+
         for (var index in day_price_array){
           var day_price_item_array = day_price_array[index].split(","); //将每个 '2016-08-24',58 类型的串分割
           var day_item = day_price_item_array[0].replace(/'/g,""); //日期  格式为 2016-08-25
+          day_item_array.push(day_item);
           var price_item = day_price_item_array[1]; //价格
-          //图表
+          price_item = Number(price_item);
+          price_item_array.push(price_item);
         }
+        console.log(price_item_array);
+        //图表
+        $('#fali_float_qushi').highcharts({
+            colors: ['#ED561B', '#DDDF00', '#058DC7', '#50B432', '#24CBE5', '#64E572',
+                '#FF9655', '#FFF263', '#6AF9C4'],
+            title: {
+                text: '近期价格趋势',
+                x: -20 //center
+            },
+            subtitle: {
+                text: '数据来源:etao.com',
+                x: -20
+            },
+            xAxis: {
+                categories: day_item_array
+            },
+            yAxis: {
+                title: {
+                    text: '价格（元）'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: '元'
+            },
+            series: [{
+              name: '价格',
+              data: price_item_array
+            }]
+        });
       }else{
         var priceExp=new RegExp("<span class='original-price del'>￥(.*?)</span>","g"),
         price = priceExp.exec(pageData);
@@ -75,19 +115,34 @@ chrome.runtime.sendMessage(
     }
 
   });
-  
+
+
 var fali_head_qushi = React.DOM.div({id:'fali_head_qushi'},'价格趋势');
 
 
 //fali_head start
 var fali_head = React.DOM.div({id:'fali_head'},fali_head_logo,fali_head_qushi); //DIV : id=fal_head ,内部包含： logo 价格趋势 普通返利 高额返利 加群赞助 5个块
 
+//fali_content start
+var fali_content = React.DOM.div({id:'fali_content'},'fali_content');
+
+//fali_float_qushi div start,浮动显示价格趋势的div块，是fali_wrapper的子块
+var fali_float_qushi = React.DOM.div({id:'fali_float_qushi'},'fali_float_qushi');
+
+//fali_float_simple_fanli div start,浮动显示普通返利的div块，是fali_wrapper的子块
+var fali_float_simple_fanli = React.DOM.div({id:'fali_float_simple_fanli'},'fali_float_simple_fanli');
+
+//fali_float_high_fanli div start,浮动显示高额返利的div块，是fali_wrapper的子块
+var fali_float_high_fanli = React.DOM.div({id:'fali_float_high_fanli'},'fali_float_high_fanli');
+
+//fali_float_qq_donate div start,浮动显示qq群和捐助的div块，是fali_wrapper的子块
+var fali_float_qq_donate = React.DOM.div({id:'fali_float_qq_donate'},'fali_float_qq_donate');
 
 var faliElements = React.createClass({
   displayName: "faliElements",
 
   render: function render() {
-    return React.createElement("div",{id: 'fali_wrapper'},fali_head);   //创建fali_wrapper 包裹DIV
+    return React.createElement("div",{id: 'fali_wrapper'},fali_head,fali_content,fali_float_qushi,fali_float_simple_fanli,fali_float_high_fanli,fali_float_qq_donate);   //创建fali_wrapper 包裹DIV
   }
 });
 //根据淘宝，天猫，阿里旅行等网址不同，查找网页对应位置添加 DIV 区块。
